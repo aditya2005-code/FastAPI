5a488e1855a01145529a90b125f8f75ce5b4b208
Aditya <2k23.csds2312133@gmail.com>
2026-03-08 22:58:50 +0530

    feat(task/model.py) - First table is created in the database(user_tasks)

--- Task_manager_app/main.py
+++ Task_manager_app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.utils.db import engine , Base
-
+from src.task.model import TaskModel
 Base.metadata.create_all(engine)
 
 app = FastAPI(title = "Task Manager App")
