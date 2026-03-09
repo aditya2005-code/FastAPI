966523c78cd1389c678c070fc84660fa0cc67651
Aditya <2k23.csds2312133@gmail.com>
2026-03-09 16:36:11 +0530

    feat(task/router) Implement api  for update task and validated with Postman

--- Task_manager_app/src/task/controller.py
+++ Task_manager_app/src/task/controller.py
@@ -1,5 +1,6 @@
 from src.task.dtos import TaskSchema
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from src.task.model import TaskModel
 
 def create_task(body: TaskSchema, db: Session):
@@ -16,3 +17,24 @@ def create_task(body: TaskSchema, db: Session):
 def get_tasks(db:Session):
     tasks = db.query(TaskModel).all()
     return {"status" : "All task" , "data":tasks}
+
+def get_one_task(task_id:int , db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        return HTTPException(404 , detail = "Task id is not present")
+    return {"status" : "Task Fetched Succesfully" , "data":one_task}
+
+def  update_task(body:TaskSchema , task_id:int , db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        return HTTPException(404 , detail = "Task id is not present")
+    
+    body = body.model_dump()
+    for field,value in body.items():
+        setattr(one_task,field,value)
+
+    db.add(one_task)
+    db.commit()
+    db.refresh(one_task)
+
+    return {"status" : "Task is updated" , "data":one_task}
--- Task_manager_app/src/task/router.py
+++ Task_manager_app/src/task/router.py
@@ -12,3 +12,12 @@ def create_task(body:TaskSchema , db:Session = Depends(get_db)):
 @task_routes.get("/all_tasks")
 def get_all_tasks(db:Session = Depends(get_db)):
     return controller.get_tasks(db)
+
+@task_routes.get("/one_task/{task_id}")
+def get_one_task(task_id : int , db:Session=Depends(get_db)):
+    return controller.get_one_task(task_id,db)
+
+
+@task_routes.put("/update_task/{task_id}")
+def update_task(body : TaskSchema ,task_id:int , db:Session = Depends(get_db)):
+    return controller.update_task(body,task_id,db)
