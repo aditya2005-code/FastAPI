992ea640ed37de6984f042a3025b99e7bb058e76
Aditya <2k23.csds2312133@gmail.com>
2026-03-14 20:17:07 +0530

    feat(user) add route for the user

--- Task_manager_app/main.py
+++ Task_manager_app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from src.utils.db import engine , Base
 from src.task.model import TaskModel
 from src.task.router import task_routes
+from src.user.router import user_routes
 
 Base.metadata.create_all(engine)
 
 app = FastAPI(title = "Task Manager App")
 app.include_router(task_routes)
+app.include_router(user_routes)
